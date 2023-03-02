81264c008a74611dd0ac4a73af875a3122db66d2
Jiazhou Chen <rgagechen@gmail.com>
2023-03-02 13:32:18 +0000

    update

--- LT_ST_analysis.R
+++ LT_ST_analysis.R
@@ -2,9 +2,13 @@ require("lmerTest")
 require("lme4")
 require("ggplot2")
 
+####Data Path: 
+#Expecting a string of path that points to the folder with *.csv files in them. 
+dt_path <- "/Users/jiazhouchen/Documents/NIH-UCL/RCode/LetterTask_JC/data/LT_SF"
+
 ##following section is for loading data
-if(FALSE) {
-  ls_csvs <- list.files(path = "data/LT_SF",pattern = "*.csv", recursive = F,include.dirs = T,full.names = T)
+if(!file.exists('LT_SF_analysis.rdata')) {
+  ls_csvs <- list.files(path = dt_path,pattern = "*.csv", recursive = F,include.dirs = T,full.names = T)
   all_data <- lapply(ls_csvs,read.csv,stringsAsFactors = F)
   for (x in 1:length(all_data)){
     all_data[[x]]$uq_index <- x
@@ -123,6 +127,7 @@ if(FALSE) {
   bdf$par_type <- FALSE
   
   cdf <- rbind(bdf,edf)
+  save(sum_df,bdf,edf,cdf,file = "LT_SF_analysis.rdata")
 } else {
   load("LT_SF_analysis.rdata")
 }
@@ -131,8 +136,9 @@ axis_style <- element_text(face = "bold.italic", color = "black", size = 20)
 ag_cut<-aggregate(TargetResp.corr~N+uq_index,data = bdf,FUN = mean)
 id_cut<-ag_cut$uq_index[which(ag_cut$N==3 & ag_cut$TargetResp.corr < 0.3) ]
 bdf$id_cut <- bdf$uq_index %in% id_cut
+bdf$participant <- as.factor(bdf$uq_index)
 bdf$mood_dmean<-do.call(c,lapply(split(bdf$slider.response,bdf$uq_index),function(x){as.numeric(scale(x,scale = T,center=T))}))
-stop("")
+stop("Finished Importing")
 ###Overview:
 ######Objects:
 ###sum_df: data.frame object that contains summary stats on all participants (both included / excluded). Participants without IDs are supplied with one starting wtih "NOID_"
@@ -153,13 +159,17 @@ stop("")
 ###fb_value: the accuracy percentage when participants view the feedback, 7 per participants, duplicated for all trials following each feedback;
 ###par_type: TRUE for participants who would otherwise be excluded based on accurate trial number criteria. one per participant;
 ###fb_tt5: indicator for feedback trials and 5 after them. Used mostly to exclude trials under the influence of feedback. 
+
+###Raw data graphs:
+
+#Mood by time; aggregrate
 ggplot(bdf,aes(y=slider.response,x=trial),color="#85d4c7") + 
   geom_point(position = position_dodge2(width = 1),color="#85d4c7",alpha=0.1)+
   geom_line(mapping = aes(fill=participant,color=participant),stat="smooth",method = "lm", size = 1.5,alpha = 0.4, color="#85d4c7")+
   stat_summary(fun.data=mean_sdl, fun.args = list(mult=1), geom="errorbar", color="#0c4a40", width=0.1, size =0.5) +
   geom_smooth(method="lm",color="#0c453c",fill="#0c4a40") + ylim(c(0,10.5)) +
   xlab("Trial") + ylab("Momentary Happiness Ratings")+ theme(text  = element_text(size = 20)) 
-bdf$participant <- bdf$uq_index
+
 ### Logistic regression for accuracy:
 accuracy_m1<-lme4::glmer(TargetResp.corr ~ scale(N)*scale(trial)+(scale(N)*scale(trial)|uq_index),data = bdf,
                        family = binomial(),lme4::glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 100000)))
@@ -175,33 +185,8 @@ ggplot(bdf[!is.na(bdf$TargetResp.rt),],aes(x=N,y=TargetResp.corr),color="#284E57
   theme(text  = element_text(size = 20),axis.text = axis_style,legend.position = "none") 
 ggsave(filename = "S2AxL.png",scale = 1.3,width=3,height = 4,units = "in")
 
-##Investigate the small cluster of ppl in 3
 
-ag_cut<-aggregate(TargetResp.corr~N+uq_index,data = bdf,FUN = mean)
-id_cut<-ag_cut$uq_index[which(ag_cut$N==3 & ag_cut$TargetResp.corr < 0.3) ]
-bdf$id_cut <- bdf$uq_index %in% id_cut
-ggplot(bdf,aes(x=N,y=TargetResp.corr),color="#284E57")+
-  #geom_histogram(data = aggregate(TargetResp.corr~N,data = bdf,FUN = mean),stat = "identity",alpha=0.2,fill="#0c4a40")+
-  geom_point(data=aggregate(TargetResp.corr~N+uq_index,data = bdf[!bdf$id_cut,],FUN = mean),color="#284E57",alpha=0.1,position = position_jitter(width = 0.2))+
-  geom_point(data=aggregate(TargetResp.corr~N+uq_index,data = bdf[bdf$id_cut,],FUN = mean),
-             aes(x=N,y=TargetResp.corr,color=uq_index),
-             alpha=0.4,position = position_jitter(width = 0.2))+
-  geom_line(data=aggregate(TargetResp.corr~N+uq_index,data = bdf[bdf$id_cut,],FUN = mean),
-             aes(x=N,y=TargetResp.corr,color=uq_index),
-             alpha=0.4)+
-  geom_point(data=aggregate(TargetResp.corr~N,data = bdf,FUN = mean),color="#284E57",size=5)+
-  geom_line(data=aggregate(TargetResp.corr~N,data = bdf,FUN = mean),color="#284E57",linewidth=2)+
-  stat_summary(fun.data=mean_se, fun.args = list(mult=10), geom="errorbar", color="#0c4a40", width=0.2, size =1) +
-  scale_x_continuous(breaks = 3:9,labels=c("3","4","5","6","7","8","9"))+
-  xlab("Number of Letters") + ylab("Response Accuracy (%)") + 
-  theme(text  = element_text(size = 20),axis.text = axis_style,legend.position = "none") 
-ag_cut<-aggregate(TargetResp.corr~N+uq_index,data = bdf,FUN = sum)
-ag_cut_a<-ag_cut[which(ag_cut$uq_index %in% id_cut),]
-ag_cut_b<-reshape2::dcast(ag_cut_a,uq_index~N,value.var = "TargetResp.corr")
-ag_cut_b$tot<-apply(ag_cut_b[2:ncol(ag_cut_b)],1,sum)
-ag_cut_c <- apply(ag_cut_b[2:ncol(ag_cut_b)],1,function(x){x[2:8]/x[9]})
-
-###
+###Number of each num keys by difficulty, show some pressed "impossible" keys
 table(bdf$TargetResp.keys[bdf$id_cut],bdf$N[bdf$id_cut])
 ###Look into chance discounting:
 dist_rate <- 1/1:9 
@@ -224,10 +209,6 @@ ggsave(filename = "S2AAFxL.png",scale = 1.3,width=6,height = 4,units = "in")
 
 summary(sapply(split(bdf$TargetResp.keys,bdf$uq_index),sd,na.rm=T))
 
-### Maybe just plot the whole thing?
-summary(mood_Full) -> dfa
-as.data.frame(dfa$coefficients) -> dfa
-
 ##Plot the mood by the N:
 ggplot(bdf,aes(x=N,y=mood_dmean),color="#b181b3")+
   #geom_histogram(data = aggregate(TargetResp.corr~N,data = bdf,FUN = mean),stat = "identity",alpha=0.2,fill="#0c4a40")+
@@ -240,89 +221,11 @@ ggplot(bdf,aes(x=N,y=mood_dmean),color="#b181b3")+
   theme(text  = element_text(size = 20),axis.text = axis_style,legend.position = "none") 
 ggsave(filename = "S2MxN.png",scale = 1.3,width=3,height = 4,units = "in")
 
-ggplot(bdf,aes(x=(N),y=slider.response))+ geom_point(alpha=0.1,position = position_dodge2(width = 0.7),color = "rosybrown") + geom_smooth(method = "glm",color="darkred") + 
-  ggtitle("Happiness Rating by Difficulty (All Trials)") + xlab("Difficulty (Number of Letters)") + ylab("Happiness Rating")
-ggplot(bdf,aes(x=(N),y=slider.response)) + geom_smooth(method = "glm",color="darkred") + 
-  ggtitle("Happiness Rating by Difficulty (All Trials)") + xlab("Difficulty (Number of Letters)") + ylab("Happiness Rating")
-
-ggplot(bdf,aes(y=slider.response,x=N),color="#a493b5") + 
-  geom_point(position = position_dodge2(width = 1),color="#a493b5",alpha=0.1)+
-  geom_line(mapping = aes(fill=uq_index,color=uq_index),stat="smooth",method = "lm", size = 1.5,alpha = 0.3, color="#a493b5")+
-  stat_summary(fun.data=mean_cl_boot, fun.args = list(), geom="errorbar", color="#231c2b", width=0.2, size =1) +
-  geom_smooth(method="lm",color="#231c2b",fill="#231c2b") +
-  xlab("Number of Letters") + ylab("Momentary Happiness Ratings") + theme(text  = element_text(size = 20)) 
-
-###Plot the mood by N but without feedback influence
-ggplot(bdf[bdf$fb_tt5==0,],aes(x=(N),y=slider.response))+ geom_point(alpha=0.1,position = position_dodge2(width = 0.7),color = "tan") + geom_smooth(method = "glm",color="tan3") + 
-  ggtitle("Happiness Rating by Difficulty (No Feedback)") + xlab("Difficulty (Number of Letters)") + ylab("Happiness Rating")
-ggplot(bdf[bdf$fb_tt5==0,],aes(x=(N),y=slider.response)) + geom_smooth(method = "glm",color="tan3") + 
-  ggtitle("Happiness Rating by Difficulty (No Feedback)") + xlab("Difficulty (Number of Letters)") + ylab("Happiness Rating")
-
-ggplot(bdf[bdf$fb_tt5==0,],aes(y=slider.response,x=N),color="#a493b5") + 
-  geom_point(position = position_dodge2(width = 1),color="#a493b5",alpha=0.1)+
-  geom_line(mapping = aes(fill=uq_index,color=uq_index),stat="smooth",method = "lm", size = 1.5,alpha = 0.3, color="#a493b5")+
-  stat_summary(fun.data=mean_cl_boot, fun.args = list(), geom="errorbar", color="#231c2b", width=0.2, size =1) +
-  geom_smooth(method="lm",color="#231c2b",fill="#231c2b") +
-  xlab("Number of Letters") + ylab("Momentary Happiness Ratings") + theme(text  = element_text(size = 20)) 
-
-
-###Plot mood by trials since last feedback 
-ggplot(bdf,aes(x=(nt_sfb),y=slider.response))+ geom_smooth(method = "glm") + 
-  ggtitle("Happiness Rating by Number of Trials Since Last Feedback (starting at 0): Fitted Smooth Line Only") + xlab("Number of Trials Since Last Feedback") + ylab("Happiness Rating") 
-ggplot(bdf,aes(x=(nt_sfb),y=slider.response))+ geom_smooth(method = "glm") + geom_point(alpha=0.2) + 
-  ggtitle("Happiness Rating by Number of Trials Since Last Feedback (starting at 0)") + xlab("Number of Trials Since Last Feedback") + ylab("Happiness Rating")
-
-ggplot(bdf,aes(y=slider.response,x=nt_sfb),color="#a493b5") + 
-  geom_point(position = position_dodge2(width = 1),color="#cfd8b2",alpha=0.1)+
-  geom_line(mapping = aes(fill=uq_index,color=uq_index),stat="smooth",method = "lm", size = 1.5,alpha = 0.4, color="#71a376")+
-  stat_summary(fun.data=mean_cl_boot, fun.args = list(), geom="errorbar", color="#2b4c41", width=0.2, size =1) +
-  geom_smooth(method="lm",color="#2b4c41",fill="#2b4c41") +
-  xlab("Happiness Rating by Number of Trials Since Last Feedback (starting at 0)") + ylab("Momentary Happiness Ratings") + theme(text  = element_text(size = 20)) 
-
-ggplot(bdf,aes(x=nt_sfb,y=mood_dmean),color="#b181b3")+
-  #geom_histogram(data = aggregate(TargetResp.corr~N,data = bdf,FUN = mean),stat = "identity",alpha=0.2,fill="#0c4a40")+
-  geom_point(data=aggregate(mood_dmean~nt_sfb+uq_index,data = bdf,FUN = mean),color="#b181b3",alpha=0.2,position = position_jitter(width = 0.2))+
-  geom_point(data=aggregate(mood_dmean~nt_sfb,data = bdf,FUN = mean),color="#634a63",size=5)+
-  geom_line(data=aggregate(mood_dmean~nt_sfb,data = bdf,FUN = mean),color="#634a63",size=2)+
-  stat_summary(fun.data=mean_se, fun.args = list(mult=10), geom="errorbar", color="#634a63", width=0.3, size =1) +
-  ylim(2,-2)+
-  #scale_x_continuous(breaks = 3:9,labels=c("3","4","5","6","7","8","9"))+
-  xlab("Number of Letters") + ylab("Happiness Rating (Z-score)") + 
-  theme(text  = element_text(size = 20),axis.text = axis_style,legend.position = "none") 
-ggsave(filename = "S2MxN.png",scale = 1.3,width=3,height = 4,units = "in")
-##Plot mood by trial 
-ggplot(bdf,aes(y=slider.response,x=trial),color="#85d4c7") + 
-  geom_point(position = position_dodge2(width = 1),color="#85d4c7",alpha=0.1)+
-  geom_line(mapping = aes(fill=participant,color=participant),stat="smooth",method = "lm", size = 1.5,alpha = 0.4, color="#85d4c7")+
-  stat_summary(fun.data=mean_cl_boot, fun.args = list(), geom="errorbar", color="#0c4a40", width=0.2, size =0.5) +
-  geom_smooth(method="lm",color="#0c453c",fill="#0c4a40") + ylim(c(0,10.5)) +
-  xlab("Trial") + ylab("Momentary Happiness Ratings")+ theme(text  = element_text(size = 20)) 
-
 ###Feedback accuracy hist 
 ggplot(bdf,aes(x=fb_value))+geom_histogram()+ggtitle("Accuracy Rate at Each Feedback Histogram") + ylab("Count") + xlab("Accuracy Rate")
 ###Feedback accuracy time effect?
 ggplot(bdf[bdf$fb_zone %in% 1:6,],aes(x=fb_value,fill=as.factor(fb_zone)))+geom_histogram(bins=40)+
   ggtitle("Accuracy Rate at Each Feedback Histogram") + ylab("Count") + xlab("Accuracy Rate") + scale_fill_discrete(name = "Number of Feedback")
-###Feedback accuracy by feedback type (above average or not)
-ggplot(bdf,aes(x=(nt_sfb),y=slider.response,color = fb_value > mean(fb_value,na.rm=T)))+ geom_point(alpha = 0.2) + geom_smooth(method = "glm") + 
-  ggtitle("Happiness Rating by Number of Trials Since Last Feedback, split by Feedback Type") +
-  xlab("Number of Trials Since Last Feedback") + ylab("Happiness Rating") + scale_color_discrete(name = "Positive Feedback")
-
-
-
-##maybe a more accurate way is to do lm for everyone
-sp_bdf <- split(bdf,bdf$uq_index)
-dfxy<-do.call(rbind,lapply(1:length(sp_bdf),function(i){
-  x <- sp_bdf[[i]]
-  as.data.frame(summary(lm(slider.response~scale(N)+scale(nt_sfb)+scale(trial),data = x))$coefficients) -> xy
-  xy$variable <- c("Intercept","N","NTSFB","Trial")
-  rownames(xy) <- NULL
-  xy$uq_sub <- i
-  return(xy)
-}))
-ggplot(dfxy[dfxy$variable!="Intercept",],aes(x=Estimate)) + geom_histogram(bins = 50)+facet_wrap(~variable)
-
-
 
 ###Regression Model:
 ##(1) simple model with only trials and missed
@@ -334,33 +237,10 @@ mood_N<-lmerTest::lmer(slider.response~scale(N)+scale(trial)+missed+(scale(N)+sc
 ##(3) Full model: Add influence of trials since last feedback
 mood_Full<-lmerTest::lmer(slider.response~scale(N)+fb_yn+scale(nt_sfb)+scale(trial)+(scale(N)+fb_yn+scale(nt_sfb)+scale(trial)|uq_index),
                         data = bdf,control = lme4::lmerControl(optimizer = "bobyqa"))
-##(4) Add "I feel like getting it wrong" effect 
-mood_SF_cor<-lmerTest::lmer(slider.response~scale(N)+scale(trial)+scale(nt_sfb)+as.factor(TargetResp.corr==0)+missed+(scale(N)+scale(trial)+scale(nt_sfb)+as.factor(TargetResp.corr==0)+missed|uq_index),
-                        data = bdf,control = lme4::lmerControl(optimizer = "bobyqa"))
 ##(5) Full model [3] but on trials without feedback influence (feedback + 5 t after)
 mood_SF_noFB<-lmerTest::lmer(slider.response~scale(N)+scale(trial)+scale(nt_sfb)+missed+(scale(N)+scale(trial)+scale(nt_sfb)+missed|uq_index),
                         data = bdf[bdf$fb_tt5==0,],control = lme4::lmerControl(optimizer = "bobyqa"))
-##(6) Full model [3] but 1) on full data including otherwise excluded participants based on accuracy; 
-#########################2) included term to differentiate different types of participants (split by accurate trial number > 49)
-mood_SF_cb<-lmerTest::lmer(slider.response~scale(N)*par_type+scale(trial)+scale(nt_sfb)+missed+
-                             (scale(N)*par_type+scale(trial)+scale(nt_sfb)+missed|uq_index),
-                           data = cdf,control = lme4::lmerControl(optimizer = "bobyqa"))
-##(7) Same as (6) but on trials without influence of feedback only
-mood_SF_cbnoFB<-lmerTest::lmer(slider.response~scale(N)*par_type+scale(trial)+scale(nt_sfb)+missed+
-                                          (scale(N)*par_type+scale(trial)+scale(nt_sfb)+missed|uq_index),
-                        data = cdf[cdf$fb_tt5==0,],control = lme4::lmerControl(optimizer = "bobyqa"))
-##(8) Look into whether objective correctness has anything to do with mood
-bdf$correct <- factor(bdf$TargetResp.corr)
-mood_SF_objcor<-lmerTest::lmer(slider.response~scale(N)*correct+scale(trial)+scale(nt_sfb)+missed+(scale(N)*correct+scale(trial)+scale(nt_sfb)+missed|uq_index),
-                          data = bdf,control = lme4::lmerControl(optimizer = "bobyqa"))
-##(9) The investigation on individual differences in performance
-bdf$act <- scale(sum_df$act)[match(bdf$uq_index,sum_df$uq_index)]
-bdf$acc_per <- scale(sum_df$acc_per)[match(bdf$uq_index,sum_df$uq_index)]
-bdf$err_per <- scale((210 - sum_df$act) / 210)[match(bdf$uq_index,sum_df$uq_index)]
-mood_SF_idp<-lmerTest::lmer(slider.response~scale(N)*err_per+scale(trial)+scale(nt_sfb)+missed+(scale(N)*err_per+scale(trial)+scale(nt_sfb)+missed|uq_index),
-                               data = bdf,control = lme4::lmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 100000)))
-
-##(10) Early/mid/late portion of the task's effect; to show that later on without reward, the participants are more likely to 
+##(10 in Supplement) Early/mid/late portion of the task's effect; to show that later on without reward, the participants are more likely to 
 bdf$stage <- ceiling(bdf$trial / (max(bdf$trial)/3)  )-1
 bdf$stage_fa <- as.factor(bdf$stage)
 bdf$N_s <- scale(bdf$N)
@@ -373,7 +253,6 @@ mood_SF_stg_fa<-lmerTest::lmer(slider.response~scale(N)*stage_fa++scale(nt_sfb)+
 mood_SF_trial <- lmerTest::lmer(slider.response~scale(N)*scale(trial)+scale(nt_sfb)+missed+
                                   (scale(N)*scale(trial)+scale(nt_sfb)+missed|uq_index),
                                 data = bdf,control = lme4::lmerControl(optimizer = "bobyqa"))
-
 
 ggplot(bdf[!is.na(bdf$TargetResp.corr),],aes(x=N,y=TargetResp.corr,color=stage_fa))+
   stat_summary(fun.data=mean_cl_boot, fun.args = list(), geom="point", size =4) +
@@ -406,11 +285,7 @@ ggplot(bdf,aes(x=trial,y=mood_dmean),color="#d1c84f")+
   #scale_color_manual(name = "Stages", labels = c("Early", "Mid","Late"), values=c("#d7de81","#cbd453","#565c0e")) +  
   theme(text  = element_text(size = 20),axis.text = axis_style) 
 ggsave(filename = "S1_t_moodSmooth.png",scale = 1.3,width=5,height = 3,units = "in")
-##(11) Exp N
-bdf$N_scl_sq <- scale(bdf$N)^2
-mood_SF_sq<-lmerTest::lmer(slider.response~scale(N)+N_scl_sq+scale(nt_sfb)+missed+
-                              (scale(N)+N_scl_sq+scale(nt_sfb)+missed|uq_index),
-                            data = bdf[bdf$fb_tt5==0,],control = lme4::lmerControl(optimizer = "bobyqa"))
+
 
 ##Graphing the results:
 coefficients(mood_Full)$uq_index -> dfx
@@ -440,7 +315,7 @@ ggplot(dfz[dfz$variable!="Intercept",],aes(x=variable,y=Estimate,color=variable)
 ggsave(filename = "S2Regression.png",scale = 1.3,width=2522,height = 1782,units = "px")
 
 
-
+###Supplement Result: RT Analysis
 bdf$TargetResp.rt
 bdf$rt_log <- log(bdf$TargetResp.rt)
 bdf$rt_scale <- unlist(lapply(split(bdf$TargetResp.rt,bdf$uq_index),scale))
--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ## Experiment Code Availability:
 For code to recreate the experiments, see "Experiment" folder. The Letter Task with Multiple Attempt (LT_MT) is available in the LT_MT_Task folder and the Letter Task with Single Attempt (LT_ST) is available in the LT_ST_Task folder. 
 ## Data Availability:
-The data for both of the studies are available at the [OSF project page](https://osf.io/3txm5)
+The data for both of the studies are available at the [OSF project page](https://osf.io/3txm5).
+Data processing is included in the Analysis Scripts described below. Some data files were edited to remove personal information (non-identifying names, a proportion of participants had reported their name in the ID field despite the clear instruction conveying otherwise). They were assigned new ID that started with the letter "B." Certain individuals also did not provide their MTurk worker ID as instructed, they were assigned a random ID during analysis (simple non-repeated numeric numbers, done as part of data importation process). ID started with the letter "A" were participants' MTurk worker IDs. 
 
 ## Letter Task Multiple Attempt Version Analysis 
  To read more about the task, please refer to the paper.
@@ -13,11 +14,8 @@ The data for both of the studies are available at the [OSF project page](https:/
  You can load the existing data or import from raw data directly. Source the script above to load the data into your environment (must first configure path to data folder)
  ###Overview of objects in existing data:
 
- 1. Objects:
-     - **sum_df**: data.frame object that contains summary stats on all participants (both included / excluded). Participants without IDs are supplied with one starting wtih "NOID_"
-     - **bdf**: data.frame object containing filtered participants data (only accurate trial number > 49)
-     - **edf**: data.frame object containing participant data that is not in bdf but passed basic filtering (missed < 200 trials and experienced > 200 trials)
-     - **cdf**: data.frame object that's joint of bdf and edf.
+ 1. data.frames:
+     - **bdf**: data.frame object containing filtered participants data (average number of error < 5.86)
  2. Variables:
      - **slider.response**: momentary happiness rating, one observation per trial per participant;
      - **slider.rt**: reaction time of slider.response;
@@ -25,13 +23,11 @@ The data for both of the studies are available at the [OSF project page](https:/
      - **TargetResp.rt**: reaction time of participants responses, one observation per trial per participant;
      - **missed**: indicator for no reponses recorded. TRUE for missed trial. one observation per trial per participant;
      - **N**: number of letters, task contingency, one observation per trial, range from 3 to 9;
-     - **fb_yn**: indicator for if the trial contains feedback, 7 per participants
+     - **rewMag**: reward magnitude measures, range from 10 to 90;
+     - **nattempt**: number of attempts that took the participant to complete this trial.
      - **trial**: number of trial, one observation per trial, identical across participants
-     - **uq_index**: index for a unique participant
-     - **nt_sfb**: number of trials since last feedback, NA indicates no feedback seen yet. one obverstaion per trial, identical across participants
-     - **fb_value**: the accuracy percentage when participants view the feedback, 7 per participants, duplicated for all trials following each feedback;
-     - **par_type**: TRUE for participants who would otherwise be excluded based on accurate trial number criteria. one per participant;
-     - **fb_tt5**: indicator for feedback trials and 5 after them. Used mostly to exclude trials under the influence of feedback. 
+     - **uq_index**: index for a unique participant.
+     - **baseline_mood**: mood rating measured at baseline. 
  3. Regression Models:
      - See in-script comments
 
